3d6ba275037d48e0e2afadd729e50759c1d98550
twak <twakered@gmail.com>
2024-08-07 23:26:19 +0100

    script to load

--- api/app.py
+++ api/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 import logging
 import psycopg2
 from shapely.geometry import LineString
 from shapely import wkb
 import urllib.request
 import json
+# import requests
 
 app = Flask(__name__)
 app.debug = True
@@ -16,6 +17,21 @@ with open ("api/pwd.json") as fp:
 
 conn = psycopg2.connect(dbname=pwp['dbname'], user=pwp['user'], password=pwp['password'], host=pwp['host'])
 curs = conn.cursor()
+
+def get_nsew(other=[],opt={}):
+    vals = {}
+    for x in ['n', 'w', 's', 'e']+other:
+        if not x in request.args:
+            return f"missing parameters {x}"
+        vals[x]= request.args[x]
+
+    for x in opt.keys():
+        if x in request.args:
+            vals[x] = request.args[x]
+        else:
+            vals[x] = opt[x]
+
+    return vals
 
 @app.route('/')
 def index():
@@ -38,12 +54,7 @@ def index():
 @app.route("/v0/find-las")
 def find_las():
 
-    vals = {}
-    for x in ['n', 'w', 's', 'e']:
-        if not x in request.args:
-            return f"missing parameters {x}"
-        vals[x]= request.args[x]
-
+    vals = get_nsew()
 
     curs.execute(
         f"""
@@ -68,3 +79,23 @@ def find_las():
         out.append(x[1])
 
     return json.dumps(out)
+
+"""
+    returns a section of the orthomosaic at resolution width x height for given 27700 area
+     
+    /v0/find-png?n=601158.9&w=261757.9&s=601205.6&e=261660.2&width=100&height=200
+"""
+
+@app.route("/v0/find-png")
+def find_texture():
+
+    vals = get_nsew(opt={"width":100, "height":100})
+    for k in vals.keys():
+        print(f"{k} {vals[k]}")
+
+    return redirect(f"http://dt.twak.org:8080/geoserver/ne/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&"
+                        f"FORMAT=image%2Fpng&TRANSPARENT=true&STYLES&LAYERS=ne%3Ageotiffs&exceptions=application%2Fvnd.ogc.se_inimage&"
+                        f"SRS=EPSG%3A27700&WIDTH={vals['width']}&HEIGHT={vals['height']}"
+                        f"&BBOX={vals['n']}%2C{vals['e']}%2C{vals['s']}%2C{vals['w']}", code=302)
+
+
