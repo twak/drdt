a5a7bf5dabc1a00f3ef9ac4cb67bbbde21c596f7
twak <twakered@gmail.com>
2024-11-11 14:20:58 +0000

    ....

---
+++ simple_tarmac/build_simple.py
@@ -0,0 +1,14 @@
+
+# for each direction line
+
+# subdivide into 10m segments
+
+# compute wedges for each segment
+
+# compute 'quad' for start and end of each segment
+
+# for each segment
+
+# define a grid of points + sample to build grid
+
+# build a mesh, download textures
